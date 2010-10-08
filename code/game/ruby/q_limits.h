e9c36daa4615d84c0ae43c827723bc83dd2e3b1a
Dario <darios3@gmail.com>
2010-10-08 20:55:18 -0300

    macros of q_limits.h complete (all macros used in numeric.c)

--- code/game/ruby/q_limits.h
+++ code/game/ruby/q_limits.h
@@ -1,4 +1,14 @@
 #ifndef __LIMITS_H
 #define __LIMITS_H
 
+#define LONG_MAX	2147483647L
+#define LONG_MIN	(-LONG_MAX - 1L)
+
+#define CHAR_BIT	8
+
+#define INT_MIN	(-INT_MAX - 1)
+#define INT_MAX	2147483647
+
+#define UINT_MAX	4294967295U
+
 #endif
