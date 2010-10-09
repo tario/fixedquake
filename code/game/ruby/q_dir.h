c25f68c2bfda0b943ff0cd81b254530237e549f4
Dario <darios3@gmail.com>
2010-10-09 16:31:54 -0300

    more defines and structs for q_dir.h

--- code/game/ruby/q_dir.h
+++ code/game/ruby/q_dir.h
@@ -5,10 +5,17 @@ typedef struct ST_DIR {
 	int __dummy;
 } DIR;
 
+struct stat {
+	int st_mode;
+};
+
 struct dirent {
 	char* d_name;
 };
 
 typedef long int off_t;
+
+#define S_IFMT 64
+#define S_IFDIR 65
 
 #endif
